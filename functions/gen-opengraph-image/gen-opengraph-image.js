fc63f3727a54e2ff4e48ba25cbdb8026d78881ae
Will <w.alexander.johnson@gmail.com>
2020-04-17 20:47:13 -0500

    changed view size

--- functions/gen-opengraph-image/gen-opengraph-image.js
+++ functions/gen-opengraph-image/gen-opengraph-image.js
@@ -1,9 +1,15 @@
 const playwright = require("playwright-aws-lambda");
+const fs = require("fs");
+const script = fs.readFileSync("./image.js", "utf-8");
 
 exports.handler = async function(event, ctx) {
   const browser = await playwright.launchChromium();
   const context = await browser._defaultContext;
   const page = await context.newPage();
+  page.setViewportSize({
+    width: 1200,
+    height: 630
+  });
   await page.setContent(`<!DOCTYPE html>
   <html>
     <head>
@@ -11,10 +17,11 @@ exports.handler = async function(event, ctx) {
     </head>
   
     <body>
-      <div id="corgi"><div>CORGIIIS</div></div>
+      <div id="corgi"><div>NO CORGIS HERE</div></div>
     </body>
   </html>
   `);
+  await page.addScriptTag({ content: script });
   const boundingRect = await page.evaluate(() => {
     const corgi = document.getElementById("corgi");
     const { x, y, width, height } = corgi.children[0].getBoundingClientRect();
